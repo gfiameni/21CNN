0f70136e67c7bd4582cbd25b9c5191ea6ae2cd89
dprelogo <david.prelogovic@gmail.com>
2019-09-24 19:54:59 +0200

    create Database class and other utilities

---
+++ CreateDataSet.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys
+
+import DatabaseUtils
+
+
+#define path to database, send to program as parameters if different from default
+
+if len(sys.argv) == 1:
+    BoxesPath = "/amphora/bradley.greig/21CMMC_wTs_LC_RSDs_Nicolas/Programs/LightConeBoxes"
+else:
+    BoxesPath = sys.argv[1]
+if len(sys.argv) <= 2:
+    ParametersPath = "/amphora/bradley.greig/21CMMC_wTs_LC_RSDs_Nicolas/Programs/GridPositions"
+else:
+    ParametersPath = sys.argv[2]
+
+Redshifts = ['006.00060', '006.75589', '007.63960', '008.68274', '009.92624', '011.42503',
+            '013.25424', '015.51874', '018.36856', '022.02434', '026.82138', '033.28927', '034.50984']
+    
+
+database = DatabaseUtils.Database(BoxesPath, ParametersPath, Redshifts)
---
+++ DatabaseUtils.py
@@ -0,0 +1,52 @@
+
+
+class Database:
+    def __init__(self, BoxesPath, ParametersPath, Redshifts, BoxType = "delta_T", BoxRes = 200, BoxSize = 300, WalkerID = 0, WalkerSteps = 10000):
+        self.BoxesPath = BoxesPath
+        self.ParametersPath = ParametersPath
+        self.BoxType = BoxType
+        self.BoxRes = BoxRes
+        self.BoxSize = BoxSize
+        self.WalkerID = WalkerID
+        self.Redshifts = Redshifts
+        self.WalkerSteps = WalkerSteps
+
+        self.NumBoxes = len(Redshifts) - 1
+
+    def CreateFilepath(self, RedshiftIndex, WalkerIndex):
+        filepath = f"{self.BoxesPath}/{self.BoxType}_{self.WalkerID:.6f}_{WalkerIndex:.6f}" \
+                   f"__zstart{self.Redshifts[RedshiftIndex]}_zend{self.Redshifts[RedshiftIndex + 1]}_" \
+                   f"FLIPBOXES0_{self.BoxRes}_{self.BoxSize}Mpc_lighttravel"
+        # print(filepath)
+        return filepath
+
+    def LoadBox(self, RedshiftIndex, WalkerIndex):
+        if RedshiftIndex < 0 or RedshiftIndex > self.NumBoxes:
+            raise ValueError(f"should be between 0 and {self.NumBoxes-1}")
+        if WalkerIndex < 0 or WalkerIndex > self.WalkerSteps:
+            raise ValueError(f"should be between 0 and {self.WalkerSteps - 1}")
+
+        filepath = self.CreateFilepath(RedshiftIndex, WalkerIndex)
+        
+        return np.fromfile(open(filepath,'rb'), dtype = np.dtype('float32'), \
+                    count = int(self.BoxRes)**3).reshape((int(self.BoxRes), int(self.BoxRes), int(self.BoxRes)))
+        
+    def CombineBoxes(self, WalkerIndex, NumberOfBoxes, StartIndex = 0):
+        """
+        Connecting all boxes between StartIndex and StarIndex + NumberOfBoxes
+        """
+        Box = self.LoadBox(StartIndex, WalkerIndex)
+        for i in range(StartIndex + 1, StartIndex + NumberOfBoxes):
+            #not sure about the axis = 0, 1, 2? It seems from 21cmFAST, it should be axis=0
+            Box = np.concatenate((Box, self.LoadBox(i, WalkerIndex)), axis=0) 
+        return Box
+
+def SliceBoxNTimesXY(Box, N):
+    BoxDim = Box.shape
+    slices = np.zeros((2 * N, BoxDim[1], BoxDIM[0]))
+    for x in range(N):
+        slices[x] = Box[:, x * BoxDim[1] // N, :].T
+    for y in range(N):
+        slices[y] = Box[:, :, y * BoxDIm[2] // N].T
+
+    return slices
