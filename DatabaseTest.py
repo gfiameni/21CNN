4b3e33ac3b7d4daefecdcd5dbc5a840ca0aa123e
dprelogo <david.prelogovic@gmail.com>
2019-09-28 19:23:56 +0200

    create database plot test

--- DatabaseTest.py
+++ DatabaseTest.py
@@ -1,6 +1,6 @@
 import numpy as np
 from database import DatabaseUtils
-from CNN.formatting import Filters
+import matplotlib.pyplot as plt
 
 Redshifts = ['006.00060', '006.75589', '007.63960', '008.68274', '009.92624', '011.42503', \
             '013.25424', '015.51874', '018.36856', '022.02434', '026.82138', '033.28927', '034.50984']
@@ -20,4 +20,4 @@ fig=plt.figure(figsize=(10, 10))
 for i in range(1, 10 + 1):
     fig.add_subplot(10, 1, i)
     plt.imshow(DataX[100*i, i+5], cmap="gray")
-plt.savefig('DatabaseTest'.pdf')
+plt.savefig('DatabaseTest.pdf')
