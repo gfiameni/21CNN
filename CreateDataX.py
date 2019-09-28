b66a9edb82152495e5fe60d83d9ccb3501cf6af2
dprelogo <david.prelogovic@gmail.com>
2019-09-28 19:21:44 +0200

    create database plot test

--- CreateDataX.py
+++ CreateDataX.py
@@ -21,15 +21,6 @@ Parameters = ["ZETA", "TVIR_MIN", "L_X", "NU_X_THRESH"]
 
 database = DatabaseUtils.Database(Parameters, Redshifts, BoxesPath, ParametersPath)
 
-##############TESTING##############
-# Box = database.CombineBoxes(9999, 12)
-# BoxSlices = DatabaseUtils.SliceBoxNTimesXY(Box, 5)
-# fig=plt.figure(figsize=(10, 10))
-# for i in range(1, 10 + 1):
-#     fig.add_subplot(10, 1, i)
-#     plt.imshow(BoxSlices[i-1], cmap="gray")
-# plt.savefig('/home/dprelogovic/Documents/proba_slices.pdf')
-
 TotalData = database.CreateSlicedData(SlicesPerAxis = spa)
 print(TotalData.shape)
 np.save(f"../data/database{spa}_{database.Dtype}", TotalData)
---
+++ DatabaseTest.py
@@ -0,0 +1,23 @@
+import numpy as np
+from database import DatabaseUtils
+from CNN.formatting import Filters
+
+Redshifts = ['006.00060', '006.75589', '007.63960', '008.68274', '009.92624', '011.42503', \
+            '013.25424', '015.51874', '018.36856', '022.02434', '026.82138', '033.28927', '034.50984']
+Parameters = ["ZETA", "TVIR_MIN", "L_X", "NU_X_THRESH"]
+database = DatabaseUtils.Database(Parameters, Redshifts)
+
+spa = 5
+
+DataFilepath = "../data/"
+DataXname = f"database{spa}_{database.Dtype}.npy"
+# DataYname = f"databaseParams_{database.Dtype}.npy"
+
+DataX = np.load(DataFilepath+DataXname)
+
+
+fig=plt.figure(figsize=(10, 10))
+for i in range(1, 10 + 1):
+    fig.add_subplot(10, 1, i)
+    plt.imshow(DataX[100*i, i+5], cmap="gray")
+plt.savefig('DatabaseTest'.pdf')
