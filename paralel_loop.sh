9e0b2749225bbb54940cf4f06d7d37c65c02748b
dprelogo <david.prelogovic@gmail.com>
2019-11-19 18:35:39 +0100

    bash parralelization

--- paralel_loop.sh
+++ paralel_loop.sh
@@ -1,7 +1,13 @@
-N=4
+#!/bin/bash
+task(){
+   python Create3Data.py "$1";
+   echo "$1"
+}
+
+N=100
 (
-for thing in a b c d e f g; do 
+for k in {0..9999}; do 
    ((i=i%N)); ((i++==0)) && wait
-   task "$thing" & 
+   task "$k" & 
 done
 )
