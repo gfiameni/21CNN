32468dec85f18e8a895a8d646a4eb99f56c8b0b6
dprelogo <david.prelogovic@gmail.com>
2019-11-21 11:45:45 +0100

    slice 3D data

--- small_script.py
+++ small_script.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 data = np.load("data3D_boxcar444_float32.npy")
+print(data.shape)
 total = []
 
 total.append(data[:, :25, :25, :])
@@ -9,4 +10,7 @@ total.append(data[:, :25, 25:, :])
 total.append(data[:, 25:, 25:, :])
 
 total = np.array(total, dtype=np.float32)
+print(total.shape)
+total = np.swapaxes(total, 0, 1)
+print(total.shape)
 np.save("data3D_boxcar444_sliced22_float32.npy", total)
