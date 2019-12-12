c8b69b5586153115b00b4f10065b70f46fd8e8c0
dprelogo <david.prelogovic@gmail.com>
2019-12-12 16:58:46 +0100

    write ConvRNN3D model

---
+++ build_try.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+# import keras
+from tensorflow import keras
+
+# #setting up GPU
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 1. #setting the percentage of GPU usage
+config.gpu_options.visible_device_list = "0" #for picking only some devices
+# config.gpu_options.allow_growth = True
+config.log_device_placement=True
+keras.backend.set_session(tf.Session(config=config))
+keras.backend.set_image_data_format('channels_last')
+
+import src.py21cnn.utilities as utilities
+import numpy as np
+import sys
+import importlib
+import argparse
+import itertools
+
+
+parser = argparse.ArgumentParser(prog = 'Run Model')
+parser.add_argument('--dimensionality', type=int, choices=[2, 3], default=3)
+parser.add_argument('--removed_average', type=int, choices=[0, 1], default=1)
+parser.add_argument('--Zmax', type=int, default=30)
+parser.add_argument('--data_location', type=str, default="/scratch/../../")
+parser.add_argument('--saving_location', type=str, default="/scratch/../../")
+parser.add_argument('--model', type=str, default="RNN.SummarySpace3D")
+parser.add_argument('--HyperparameterIndex', type=int, choices=range(576), default=0)
+parser.add_argument('--epochs', type=int, default=200)
+parser.add_argument('--file_prefix', type=str, default="")
+
+inputs = parser.parse_args()
+inputs.removed_average = bool(inputs.removed_average)
+inputs.model = inputs.model.split('.')
+print("INPUTS:", inputs)
+ModelClassObject = getattr(importlib.import_module(f'src.py21cnn.architectures.{inputs.model[0]}'), inputs.model[1])
+
+def leakyrelu(x):
+    return keras.activations.relu(x, alpha=0.1)
+
+HyP = {}
+HyP["Loss"] = [[None, "mse"]]
+HyP["Epochs"] = [inputs.epochs]
+HyP["LearningRate"] = [0.01, 0.001, 0.0001]
+HyP["Dropout"] = [0.2, 0.5]
+HyP["ReducingLR"] = [True]
+HyP["BatchSize"] = [20, 100]
+HyP["BatchNormalization"] = [True, False]
+HyP["Optimizer"] = [
+                    [keras.optimizers.RMSprop, "RMSprop", {}],
+                    [keras.optimizers.SGD, "SGD", {}],
+                    [keras.optimizers.SGD, "Momentum", {"momentum":0.9, "nesterov":True}],
+                    # [keras.optimizers.Adadelta, "Adadelta", {}],
+                    # [keras.optimizers.Adagrad, "Adagrad", {}],
+                    [keras.optimizers.Adam, "Adam", {}],
+                    # [keras.optimizers.Adam, "Adam", {"amsgrad":True}],
+                    [keras.optimizers.Adamax, "Adamax", {}],
+                    [keras.optimizers.Nadam, "Nadam", {}],
+                    ]
+HyP["ActivationFunction"] = [
+                            ["relu", {"activation": keras.activations.relu, "kernel_initializer": keras.initializers.he_uniform()}],
+                            # [keras.layers.LeakyReLU(alpha=0.1), "leakyrelu"],
+                            ["leakyrelu", {"activation": leakyrelu, "kernel_initializer": keras.initializers.he_uniform()}],
+                            ["elu", {"activation": keras.activations.elu, "kernel_initializer": keras.initializers.he_uniform()}],
+                            ["selu", {"activation": keras.activations.selu, "kernel_initializer": keras.initializers.lecun_normal()}],
+                            # [keras.activations.exponential, "exponential"],
+                            # [keras.activations.tanh, "tanh"],
+                            ]
+
+
+HyP_list = list(itertools.product(*HyP.values()))
+HP_dict = dict(zip(HyP.keys(), HyP_list[inputs.HyperparameterIndex]))
+HP = utilities.AuxiliaryHyperparameters(**HP_dict)
+
+Data = utilities.Data(filepath=inputs.data_location, 
+                      dimensionality=inputs.dimensionality, 
+                      removed_average=inputs.removed_average, 
+                      Zmax=inputs.Zmax)
+# Data.loadTVT(model_type=inputs.model[0])
+
+print("HYPERPARAMETERS:", str(HP))
+print("DATA:", str(Data))
+
+ModelClass = ModelClassObject((526, 25, 25, 1), HP)
+ModelClass.build()
--- src/py21cnn/architectures/RNN.py
+++ src/py21cnn/architectures/RNN.py
@@ -78,3 +78,109 @@ class SummarySpace3D:
         self.model = keras.models.Model(inputs = img_input, outputs = x, name= "RNN_SummarySpace3D")
         print(self.model.summary())
         return self.model
+
+
+
+class ConvRNN3D:
+    def __init__(self, 
+                InputShape, 
+                # Data, 
+                AuxiliaryHP, 
+                # RNNLayer = keras.layers.CuDNNLSTM,
+                RNNLayer = CuDNNLSTM,
+                RNNsizes = [128, 64, 64],
+                FCsizes = [32, 32, 16, 8],
+                ):
+        self.InputShape = InputShape
+        # self.Data = Data
+        self.AuxHP = AuxiliaryHP
+        self.RNNLayer = RNNLayer
+        self.RNNsizes = RNNsizes
+        self.FCsizes = FCsizes
+        if self.AuxHP.ActivationFunction[0] == "selu":
+            self.DropoutLayer = keras.layers.AlphaDropout
+        else:
+            self.DropoutLayer = keras.layers.Dropout
+    
+    # def conv2d_bn(self, x, filters, kernel_size, padding="valid", strides=(1, 1)):
+    #     x = keras.layers.TimeDistributed(keras.layers.Conv2D(filters, 
+    #                                                         kernel_size, 
+    #                                                         strides=strides, 
+    #                                                         padding=padding,
+    #                                                         **self.AuxHP.ActivationFunction[1]))(x)
+    #     if self.AuxHP.BatchNormalization == True:
+    #         x = keras.layers.BatchNormalization()(x)
+    #     return x
+    # def conv2d(self, x, filters, kernel_size, padding="valid", strides=(1, 1)):
+    #     x = keras.layers.TimeDistributed(keras.layers.Conv2D(filters, 
+    #                                                         kernel_size, 
+    #                                                         strides=strides, 
+    #                                                         padding=padding,
+    #                                                         **self.AuxHP.ActivationFunction[1]))(x)
+    #     return x
+
+    def build(self):
+        img_input = keras.layers.Input(shape=self.InputShape)
+
+        x = keras.layers.ConvLSTM2D(filters=64, kernel_size=(8, 8), return_sequences=True)(img_input)
+        x = keras.layers.TimeDistributed(keras.layers.MaxPooling2D(), name='pool_1')(x)
+        if self.AuxHP.BatchNormalization == True:
+            x = keras.layers.BatchNormalization()(x)
+        x = keras.layers.ConvLSTM2D(filters=128, kernel_size=(4, 4), return_sequences=True)(x)
+        x = keras.layers.TimeDistributed(keras.layers.MaxPooling2D(), name='pool_2')(x)
+        if self.AuxHP.BatchNormalization == True:
+            x = keras.layers.BatchNormalization()(x)
+        x = keras.layers.TimeDistributed(keras.layers.Flatten(), name='flatten')(x)
+        x = keras.layers.TimeDistributed(self.DropoutLayer(self.AuxHP.Dropout), name='dropout')(x)
+        x = self.RNNLayer(128, return_sequences=True)(x)
+        if self.AuxHP.BatchNormalization == True:
+            x = keras.layers.BatchNormalization()(x)
+
+        for i in self.RNNsizes:
+            x = self.RNNLayer(i, return_sequences=True)(x)
+            if self.AuxHP.BatchNormalization == True:
+                x = keras.layers.BatchNormalization()(x)
+        #first dense layer is stil RNN
+        x = self.RNNLayer(self.FCsizes[0], return_sequences=False)(x)
+        if self.AuxHP.BatchNormalization == True:
+            x = keras.layers.BatchNormalization()(x)
+
+        # x = self.DropoutLayer(self.AuxHP.Dropout)(x)
+
+        for i in self.FCsizes[1:]:
+            x = keras.layers.Dense(i, **self.AuxHP.ActivationFunction[1])(x)
+            # not doing batch norm on last layers
+
+        x = keras.layers.Dense(4)(x)
+
+        self.model = keras.models.Model(inputs = img_input, outputs = x, name= "RNN_ConvRNN3D")
+        print(self.model.summary())
+        return self.model
+
+
+class Hybrid3D:
+    def __init__(self,
+                InputShape, 
+                # Data, 
+                AuxiliaryHP, 
+                # RNNLayer = keras.layers.CuDNNLSTM,
+                RNNLayer = CuDNNLSTM,
+                RNNsizes = [128, 64, 64],
+                FCsizes = [32, 32, 16, 8],
+                ):
+        self.InputShape = InputShape
+        # self.Data = Data
+        self.AuxHP = AuxiliaryHP
+        self.RNNLayer = RNNLayer
+        self.RNNsizes = RNNsizes
+        self.FCsizes = FCsizes
+        if self.AuxHP.ActivationFunction[0] == "selu":
+            self.DropoutLayer = keras.layers.AlphaDropout
+        else:
+            self.DropoutLayer = keras.layers.Dropout        
+
+    def build(self):
+        img_input = keras.layers.Input(shape=self.InputShape)
+        x = keras.layers.Conv3D(128, (8, 8, 8), **self.AuxHP.ActivationFunction[1])(img_input)
+
+
