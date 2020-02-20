a93520155cb53e14fd3ca617ce4c1d120c586306
dprelogo <david.prelogovic@gmail.com>
2020-02-20 11:46:21 +0100

    improve plots

--- plotting.py
+++ plotting.py
@@ -8,10 +8,7 @@ from matplotlib.ticker import MaxNLocator, NullLocator, ScalarFormatter, AutoLoc
 from matplotlib.gridspec import GridSpec
 from matplotlib import transforms
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
-from scipy.stats import gaussian_kde
-from scipy.stats import kde
-from astropy.modeling import models, fitting
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 import numpy as np
 
@@ -23,18 +20,17 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 import argparse
-parser = argparse.ArgumentParser(prog = 'Run Model')
+parser = argparse.ArgumentParser(prog = 'Predict and Plot Model')
 parser.add_argument('--dimensionality', type=int, choices=[2, 3], default=3)
 parser.add_argument('--removed_average', type=int, choices=[0, 1], default=1)
 parser.add_argument('--Zmax', type=int, default=30)
 parser.add_argument('--data_location', type=str, default="../data/")
 parser.add_argument('--saving_location', type=str, default="../models/")
 parser.add_argument('--logs_location', type=str, default="../logs/")
-parser.add_argument('--model', type=str, default="RNN.SummarySpace3D")
-parser.add_argument('--HyperparameterIndex', type=int, choices=range(576), default=0)
+parser.add_argument('--pics_location', type=str, default="../pics/")
 parser.add_argument('--epochs', type=int, default=200)
-parser.add_argument('--gpus', type=int, default=1)
-parser.add_argument('--multi_gpu_correction', type=int, choices=[0, 1, 2], default=0, help="0-none, 1-batch_size, 2-learning_rate")
+parser.add_argument('--model', type=str, default="RNN.SummarySpace3D")
+parser.add_argument('--HyperparameterIndex', type=int, choices=range(32), default=0)
 parser.add_argument('--file_prefix', type=str, default="")
 
 inputs = parser.parse_args(['--model', 'CNN.basic2D', '--dimensionality', '2', '--HyperparameterIndex', '237'])
@@ -44,14 +40,13 @@ print("INPUTS:", inputs)
 
 
 import tensorflow as tf
-# #setting up GPU
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 1. #setting the percentage of GPU usage
 config.gpu_options.visible_device_list = "0" #for picking only some devices
 config.gpu_options.allow_growth = True
 # config.log_device_placement=True
-# tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
-tf.compat.v1.enable_eager_execution(config=config)
+tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
+# tf.compat.v1.enable_eager_execution(config=config)
 
 from tensorflow import keras
 keras.backend.set_image_data_format('channels_last')
@@ -71,63 +66,41 @@ def leakyrelu(x):
 HyP = {}
 HyP["Loss"] = [[None, "mse"]]
 HyP["Epochs"] = [inputs.epochs]
-HyP["BatchSize"] = [20, 100]
-HyP["LearningRate"] = [0.01, 0.001, 0.0001]
-HyP["Dropout"] = [0.2, 0.5]
+HyP["BatchSize"] = [20]
+HyP["LearningRate"] = [1e-3, 1e-4, 1e-5, 1e-6]
+HyP["Dropout"] = [0.2]
 HyP["ReducingLR"] = [True]
 HyP["BatchNormalization"] = [True, False]
 HyP["Optimizer"] = [
-                    [keras.optimizers.RMSprop, "RMSprop", {}],
-                    [keras.optimizers.SGD, "SGD", {}],
-                    [keras.optimizers.SGD, "Momentum", {"momentum":0.9, "nesterov":True}],
-                    # [keras.optimizers.Adadelta, "Adadelta", {}],
-                    # [keras.optimizers.Adagrad, "Adagrad", {}],
                     [keras.optimizers.Adam, "Adam", {}],
-                    # [keras.optimizers.Adam, "Adam", {"amsgrad":True}],
-                    [keras.optimizers.Adamax, "Adamax", {}],
                     [keras.optimizers.Nadam, "Nadam", {}],
                     ]
 HyP["ActivationFunction"] = [
                             ["relu", {"activation": keras.activations.relu, "kernel_initializer": keras.initializers.he_uniform()}],
-                            # [keras.layers.LeakyReLU(alpha=0.1), "leakyrelu"],
                             ["leakyrelu", {"activation": leakyrelu, "kernel_initializer": keras.initializers.he_uniform()}],
-                            ["elu", {"activation": keras.activations.elu, "kernel_initializer": keras.initializers.he_uniform()}],
-                            ["selu", {"activation": keras.activations.selu, "kernel_initializer": keras.initializers.lecun_normal()}],
-                            # [keras.activations.exponential, "exponential"],
-                            # [keras.activations.tanh, "tanh"],
                             ]
 
 
 HyP_list = list(itertools.product(*HyP.values()))
 HP_dict = dict(zip(HyP.keys(), HyP_list[inputs.HyperparameterIndex]))
 HP = utilities.AuxiliaryHyperparameters(**HP_dict)
-#creating HP dict for TensorBoard with only HP that are changing and only human-readable information
-HP_TensorBoard = {}
-HP_TensorBoard["Model"] = f"{inputs.model[0]}_{inputs.model[1]}"
-HP_TensorBoard["LearningRate"] = HP_dict["LearningRate"]
-HP_TensorBoard["Dropout"] = HP_dict["Dropout"]
-HP_TensorBoard["BatchSize"] = HP_dict["BatchSize"]
-HP_TensorBoard["BatchNormalization"] = HP_dict["BatchNormalization"]
-HP_TensorBoard["Optimizer"] = HP_dict["Optimizer"][1]
-HP_TensorBoard["ActivationFunction"] = HP_dict["ActivationFunction"][0]
 
 Data = utilities.Data(filepath=inputs.data_location, 
                       dimensionality=inputs.dimensionality, 
                       removed_average=inputs.removed_average, 
                       Zmax=inputs.Zmax)
 
-
-def R2(y_true, y_pred):
+def R2_tflow(y_true, y_pred):
         SS_res = keras.backend.sum(keras.backend.square(y_true-y_pred)) 
         SS_tot = keras.backend.sum(keras.backend.square(y_true - keras.backend.mean(y_true, axis=0)))
         return (1 - SS_res/(SS_tot + keras.backend.epsilon()))
     
-    
 y_true = np.load(f"{Data.filepath}Y_test_0.10_{Data.hash()}.npy")
 
 filepath = f"{inputs.saving_location}{inputs.file_prefix}{inputs.model[0]}_{inputs.model[1]}_{HP.hash()}_{Data.hash()}"
+pics_filepath = f"{inputs.pics_location}{inputs.file_prefix}{inputs.model[0]}_{inputs.model[1]}_{HP.hash()}_{Data.hash()}_"
 custom_obj = {}
-custom_obj["R2"] = R2
+custom_obj["R2"] = R2_tflow
 # custom_obj["TimeHistory"] = TimeHistory
 #if activation is leakyrelu add to custom_obj
 if HP.ActivationFunction[0] == "leakyrelu":
@@ -137,9 +110,8 @@ model.summary()
 
 X_test = np.load(f"{Data.filepath}X_test_0.10_{Data.hash()}.npy")
 X_test = X_test[..., np.newaxis]
-
 y_pred = model.predict(X_test)
-
+np.save(f"{filepath}_prediction_best.npy", y_pred)
 
 with open(f"{inputs.data_location}databaseParams_min_max.txt") as f:
     y_range = json.load(f)
@@ -191,9 +163,6 @@ print(r2_score)
 
 
 
-
-
-
 nbins = 100
 for i in range(4):
     #defining plot
@@ -238,7 +207,7 @@ for i in range(4):
     im = ax_joint.imshow(hist, origin='low', vmin = 0, vmax = 2.5, interpolation=interp, cmap=plt.cm.jet, extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
     ax_joint.set_xlabel("$%s_{%s} \,\, True$"%(y_range["LatexNames"][i], y_range["LatexUnits"][i]), fontsize = 14)
     ax_joint.set_ylabel("$%s_{%s} \,\, Pred$"%(y_range["LatexNames"][i], y_range["LatexUnits"][i]), fontsize = 14)
-    ax_joint.text(0.13, 0.95, "$R^2 = %.4f$"%(r2_score[i]), horizontalalignment='center', verticalalignment='center', transform=ax_joint.transAxes, fontsize = 16, color='white')
+    ax_joint.text(0.12, 0.95, "$R^2 = %.4f$"%(r2_score[i]), horizontalalignment='center', verticalalignment='center', transform=ax_joint.transAxes, fontsize = 16, color='white')
 
     ax_marg_x.hlines(0, minmax[0], minmax[1], colors='k')
     ax_marg_x.errorbar(centers, delta['pred'], deviation['pred'])
@@ -246,15 +215,25 @@ for i in range(4):
     ax_marg_x.set_ylabel("$\epsilon_{%s}$"%(y_range["LatexUnits"][i]), fontsize = 14)
     ax_marg_x.set_xlim(minmax[0], minmax[1])
 #     ax_marg_x.set_title("$R^2 = %.4f$"%(r2_score[i]) , fontsize = 16)
+    ax_marg_x.tick_params(direction='in')
+
+
     ax_marg_y.vlines(0, minmax[0], minmax[1], colors='k')
     ax_marg_y.errorbar(delta['true'], centers, xerr = deviation['true'])
     plt.setp(ax_marg_y.get_yticklabels(), visible=False)
     ax_marg_y.set_xlabel("$\epsilon_{%s}$"%(y_range["LatexUnits"][i]), fontsize = 14)
     ax_marg_y.set_ylim(minmax[0], minmax[1])
+    ax_marg_y.tick_params(direction='in')
+
 
     
 #     fig.suptitle(y_range["LatexNames"][i])
-#     plt.tight_layout()
-    cbar = fig.colorbar(im, ticks=[0, 1, 2], pad=0.2)
-    cbar.set_label("$\log_{10} (N_{pix} + 1)$", fontsize = 14)
-    plt.show()
+    plt.tight_layout()
+    cbaxes = inset_axes(ax_joint, width="3%", height="30%", loc=4, borderpad=0.35)
+    cbar = fig.colorbar(im, cax=cbaxes, ticks=[0, 1, 2], orientation='vertical')
+    cbaxes.tick_params(color = 'white')
+
+#     cbar = fig.colorbar(im, ticks=[0, 1, 2], pad=0.2)
+    cbar.set_label("$\log_{10} (N_{pix} + 1)$", fontsize = 14, labelpad=-40, color='white')
+    plt.savefig(pics_filepath + f'{i}.pdf')
+#     plt.show()
