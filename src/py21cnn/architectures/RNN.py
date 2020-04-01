800ef8500005f2003479be8baa31c40f606b338b
dprelogo <david.prelogovic@gmail.com>
2020-04-01 15:06:21 +0200

    add simplest SummarySpace

--- src/py21cnn/architectures/RNN.py
+++ src/py21cnn/architectures/RNN.py
@@ -23,40 +23,20 @@ class SummarySpace3D:
             self.DropoutLayer = keras.layers.AlphaDropout
         else:
             self.DropoutLayer = keras.layers.Dropout
-    
-    # def conv2d_bn(self, x, filters, kernel_size, padding="valid", strides=(1, 1)):
-    #     x = keras.layers.TimeDistributed(keras.layers.Conv2D(filters, 
-    #                                                         kernel_size, 
-    #                                                         strides=strides, 
-    #                                                         padding=padding,
-    #                                                         **self.AuxHP.ActivationFunction[1]))(x)
-    #     if self.AuxHP.BatchNormalization == True:
-    #         x = keras.layers.BatchNormalization()(x)
-    #     return x
-    # def conv2d(self, x, filters, kernel_size, padding="valid", strides=(1, 1)):
-    #     x = keras.layers.TimeDistributed(keras.layers.Conv2D(filters, 
-    #                                                         kernel_size, 
-    #                                                         strides=strides, 
-    #                                                         padding=padding,
-    #                                                         **self.AuxHP.ActivationFunction[1]))(x)
-    #     return x
 
     def build(self):
         img_input = keras.layers.Input(shape=self.InputShape)
 
         x = keras.layers.TimeDistributed(keras.layers.Conv2D(64, (8, 8), **self.AuxHP.ActivationFunction[1]), name = 'conv1')(img_input)
         x = keras.layers.TimeDistributed(keras.layers.MaxPooling2D(), name = 'maxpool1')(x)
-        if self.AuxHP.BatchNormalization == True:
-            x = keras.layers.BatchNormalization()(x)
+        if self.AuxHP.BatchNormalization == True: x = keras.layers.BatchNormalization()(x)
         x = keras.layers.TimeDistributed(keras.layers.Conv2D(128, (4, 4), **self.AuxHP.ActivationFunction[1]), name = 'conv2')(x)
         x = keras.layers.TimeDistributed(keras.layers.MaxPooling2D(), name = 'maxpool2')(x)
-        if self.AuxHP.BatchNormalization == True:
-            x = keras.layers.BatchNormalization()(x)
+        if self.AuxHP.BatchNormalization == True: x = keras.layers.BatchNormalization()(x)
         x = keras.layers.TimeDistributed(keras.layers.Flatten(), name = 'flatten')(x)
         x = keras.layers.TimeDistributed(self.DropoutLayer(self.AuxHP.Dropout), name = 'dropout')(x)
         x = keras.layers.TimeDistributed(keras.layers.Dense(128, **self.AuxHP.ActivationFunction[1]), name = 'SummarySpace')(x)
-        if self.AuxHP.BatchNormalization == True:
-            x = keras.layers.BatchNormalization()(x)
+        if self.AuxHP.BatchNormalization == True: x = keras.layers.BatchNormalization()(x)
 
         for i in self.RNNsizes:
             x = self.RNNLayer(i, return_sequences=True)(x)
@@ -100,7 +80,6 @@ class SummarySpace2D:
             self.DropoutLayer = keras.layers.AlphaDropout
         else:
             self.DropoutLayer = keras.layers.Dropout
-    
 
     def build(self):
         img_input = keras.layers.Input(shape=self.InputShape)
@@ -138,6 +117,39 @@ class SummarySpace2D:
         x = keras.layers.Dense(4)(x)
 
         self.model = keras.models.Model(inputs = img_input, outputs = x, name= "RNN_SummarySpace2D")
+        print(self.model.summary())
+        return self.model
+
+class SummarySpace3D_simple:
+    def __init__(self, 
+                InputShape, 
+                AuxiliaryHP, 
+                RNNLayer = CuDNNLSTM,
+                RNNsize = 64,
+                ):
+        self.InputShape = InputShape
+        self.AuxHP = AuxiliaryHP
+        self.RNNLayer = RNNLayer
+        self.RNNsize = RNNsize
+        if self.AuxHP.ActivationFunction[0] == "selu":
+            self.DropoutLayer = keras.layers.AlphaDropout
+        else:
+            self.DropoutLayer = keras.layers.Dropout
+
+    def build(self):
+        img_input = keras.layers.Input(shape=self.InputShape)
+
+        x = keras.layers.TimeDistributed(keras.layers.Conv2D(64, (8, 8), **self.AuxHP.ActivationFunction[1]), name = 'conv1')(img_input)
+        x1 = keras.layers.TimeDistributed(keras.layers.GlobalMaxPooling2D(), name = 'maxpool')(x)
+        x2 = keras.layers.TimeDistributed(keras.layers.GlobalAveragePooling2D(), name = 'avgpool')(x)
+        x = keras.layers.Concatenate([x1, x2], axis=-1)
+        x = keras.layers.TimeDistributed(keras.layers.Flatten(), name = 'flatten')(x)
+        if self.AuxHP.BatchNormalization == True: x = keras.layers.BatchNormalization()(x)
+        x = keras.layers.TimeDistributed(self.DropoutLayer(self.AuxHP.Dropout), name = 'dropout')(x)
+        x = self.RNNLayer(self.RNNsize, return_sequences=False)(x)
+        x = keras.layers.Dense(4)(x)
+
+        self.model = keras.models.Model(inputs = img_input, outputs = x, name= "RNN_SummarySpace3D")
         print(self.model.summary())
         return self.model
 
