e379feb499aae552fbaeb7fac543e5d0b8246a14
dprelogo <david.prelogovic@gmail.com>
2020-04-28 21:10:53 +0200

    add large hyperparameter set

--- src/py21cnn/hyperparameters.py
+++ src/py21cnn/hyperparameters.py
@@ -2,7 +2,7 @@ from tensorflow import keras
 def leakyrelu(x):
     return keras.activations.relu(x, alpha=0.1)
 
-def HP(inputs):
+def HP():
     HyP = {}
     HyP["Loss"] = [[None, "mse"]]
     HyP["BatchSize"] = [20]
@@ -17,5 +17,35 @@ def HP(inputs):
     HyP["ActivationFunction"] = [
                                 ["relu", {"activation": keras.activations.relu, "kernel_initializer": keras.initializers.he_uniform()}],
                                 ["leakyrelu", {"activation": leakyrelu, "kernel_initializer": keras.initializers.he_uniform()}],
+                                ]
+    return HyP
+
+def HP_large():
+    HyP = {}
+    HyP["Loss"] = [[None, "mse"]]
+    HyP["BatchSize"] = [20, 100]
+    HyP["LearningRate"] = [0.01, 0.001, 0.0001]
+    HyP["Dropout"] = [0.2, 0.5]
+    HyP["ReducingLR"] = [True]
+    HyP["BatchNormalization"] = [True, False]
+    HyP["Optimizer"] = [
+                        [keras.optimizers.RMSprop, "RMSprop", {}],
+                        [keras.optimizers.SGD, "SGD", {}],
+                        [keras.optimizers.SGD, "Momentum", {"momentum":0.9, "nesterov":True}],
+                        # [keras.optimizers.Adadelta, "Adadelta", {}],
+                        # [keras.optimizers.Adagrad, "Adagrad", {}],
+                        [keras.optimizers.Adam, "Adam", {}],
+                        # [keras.optimizers.Adam, "Adam", {"amsgrad":True}],
+                        [keras.optimizers.Adamax, "Adamax", {}],
+                        [keras.optimizers.Nadam, "Nadam", {}],
+                        ]
+    HyP["ActivationFunction"] = [
+                                ["relu", {"activation": keras.activations.relu, "kernel_initializer": keras.initializers.he_uniform()}],
+                                # [keras.layers.LeakyReLU(alpha=0.1), "leakyrelu"],
+                                ["leakyrelu", {"activation": leakyrelu, "kernel_initializer": keras.initializers.he_uniform()}],
+                                ["elu", {"activation": keras.activations.elu, "kernel_initializer": keras.initializers.he_uniform()}],
+                                ["selu", {"activation": keras.activations.selu, "kernel_initializer": keras.initializers.lecun_normal()}],
+                                # [keras.activations.exponential, "exponential"],
+                                # [keras.activations.tanh, "tanh"],
                                 ]
     return HyP
