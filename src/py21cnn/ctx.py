b255fd4d3211aaac2ea4dd7dcc1bf68560a1caf7
dprelogo <david.prelogovic@gmail.com>
2020-05-01 10:24:53 +0200

    change context from Namespace to module

---
+++ src/py21cnn/ctx.py
@@ -0,0 +1,17 @@
+def init():
+    global inputs, HP, Data, model
+    global filepath, logdir, main_process
+    global fit_options, compile_options, generators
+    
+    inputs = None
+    HP = None
+    Data = None
+    model = None
+
+    filepath = None
+    logdir = None
+    main_process = None
+    
+    fit_options = None
+    compile_options = None
+    generators = None
