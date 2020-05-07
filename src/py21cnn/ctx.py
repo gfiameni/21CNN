7956fd4f79bb8aca31300065a2c77216e8e12e28
dprelogo <david.prelogovic@gmail.com>
2020-05-07 15:45:39 +0200

    comment variables in context

--- src/py21cnn/ctx.py
+++ src/py21cnn/ctx.py
@@ -3,15 +3,15 @@ def init():
     global filepath, logdir, main_process
     global fit_options, compile_options, generators
     
-    inputs = None
-    HP = None
-    Data = None
-    model = None
+    inputs = None # argparse.Namespace object, with all input variables
+    HP = None # py21cnn.utilities.AuxiliaryHyperparameters object
+    Data = None # py21cnn.utilities.Data or py21cnn.utilities.LargeData object
+    model = None # keras model
 
-    filepath = None
-    logdir = None
-    main_process = None
+    filepath = None # data_location + filename for model, summary, logs
+    logdir = None # location of Tensorboard logs
+    main_process = None # bool, main process saves models, summary, logs to disk, writes to stdout
     
-    fit_options = None
-    compile_options = None
-    generators = None
+    fit_options = None # dictionary passed to model.fit, model.fit_generator
+    compile_options = None # dictionary passed to model.compile
+    generators = None # in the case of large_run, dictionary of train, validation, test generators
