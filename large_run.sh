1f792ebd4c90dddeb4fe6f78e2a79493aee84d68
dprelogo <david.prelogovic@gmail.com>
2020-06-23 15:30:43 +0200

    small change

--- large_run.sh
+++ large_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-export OMP_NUM_THREADS=6
+#export OMP_NUM_THREADS=6
 export NCCL_DEBUG=INFO
 export NCCL_IB_HCA=ipogif0
 export NCCL_IB_CUDA_SUPPORT=1
@@ -9,16 +9,18 @@ export NCCL_IB_CUDA_SUPPORT=1
 horovodrun -np 4 -H localhost:4 python large_run.py \
         --N_walker 10000 \
         --N_slice 4 \
-        --N_noise 10 \
+        --N_noise 1 \
         --noise_rolling 1 \
-        --model playground.SummarySpace3D_simple \
+        --model playground.ConvRNN3D_simple \
         --model_type RNN \
-        --HyperparameterIndex 3 \
+        --HyperparameterIndex 10 \
         --epochs 10 \
         --max_epochs 1000 \
         --gpus 4 \
+	--warmup 10 \
         --data_location /scratch/d.prelogovic/data/SKA_1000/ \
 	--saving_location /scratch/d.prelogovic/runs/models/ \
         --logs_location /scratch/d.prelogovic/runs/logs/ \
-	--verbose 1
-	--workers 24 \
+	--verbose 1 \
+	--workers 1 \
+	--load_all 1 \
